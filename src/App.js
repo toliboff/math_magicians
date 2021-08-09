3c91715d873948007924d6917dee66c53a008e92
Tolibjon <tolibjohn@gmail.com>
2021-08-09 17:23:46 +0500

    Import Calculator component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/*eslint-disable */
 import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
 class App extends React.Component {
   render() {
-    return '';
+    return <Calculator />;
   }
 }
 
