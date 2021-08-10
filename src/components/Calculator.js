f45398cbcb347ffe72f4a5059b4d4963674db440
Tolibjon <tolibjohn@gmail.com>
2021-08-10 16:24:16 +0500

    Add onClick event to buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,30 @@
 /*eslint-disable */
 import React, { Component } from 'react';
-
+import calculate from '../logic/calculator.js';
+import operate from '../logic/calculator.js';
 class Calculator extends Component {
+  constructor(props){
+    super();
+    this.state={obj:{
+      total: null,
+      next: null,
+      operation: null,
+    }}
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e){
+    const result = calculate(this.state.obj, e.target.name);
+    this.setState({obj: {...this.state.obj, ...result}});
+  }
   render() {
     const buttonsArray=['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
     return (
       <div className='Calculator'>
-        <p className = 'result'>0</p>
+        <p className = 'result'>{this.state.obj.total}</p>
         <div className='buttons'>
-          {buttonsArray.map((btn) =><button key={btn}>{btn}</button>)}
+          {buttonsArray.map((btn) =><button key={btn} onClick={(e)=>this.handleClick(e)} name={btn}>{btn}</button>)}
         </div>
         
       </div>
