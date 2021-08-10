3eb7ca9e4beb7219d118af0b65925c4d99108470
Tolibjon <tolibjohn@gmail.com>
2021-08-10 19:30:00 +0500

    Refactor Calculator.js into functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,30 @@
 /*eslint-disable */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculator.js';
 import operate from '../logic/operate.js';
-class Calculator extends Component {
-  constructor(props){
-    super();
-    this.state={obj:{
-      total: null,
-      next: null,
-      operation: null,
-    },
-    result:'',
-    }
 
-    this.handleClick = this.handleClick.bind(this);
+const Calculator = (props) => {
+  const [obj, setObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    const result = calculate(obj, e.target.name, operate);
+    setObj({...obj, ...result});
   }
 
-  handleClick(e){
-    const result = calculate(this.state.obj, e.target.name, operate);
-    this.setState({obj: {...this.state.obj, ...result}});
-  }
-
-  render() {
     const buttonsArray=['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
     return (
       <div className='Calculator'>
-        <p className = 'result'>{(this.state.obj.total || '') + (this.state.obj.operation || '') + (this.state.obj.next || '')}</p>
+        <p className = 'result'>{(obj.total || '') + (obj.operation || '') + (obj.next || '')}</p>
         <div className='buttons'>
-          {buttonsArray.map((btn) =><button key={btn} onClick={(e)=>this.handleClick(e)} name={btn}>{btn}</button>)}
+          {buttonsArray.map((btn) =><button key={btn} onClick={(e)=>handleClick(e)} name={btn}>{btn}</button>)}
         </div>
         
       </div>
     )
-  }
 }
 
 export default Calculator;
