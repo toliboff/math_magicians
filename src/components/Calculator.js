fcdcc7752c6636ed7f55efe6cd2bb6b1008accd7
Tolibjon <tolibjohn@gmail.com>
2021-08-10 17:06:22 +0500

    Add conditions to show result in output

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,6 @@
 /*eslint-disable */
 import React, { Component } from 'react';
 import calculate from '../logic/calculator.js';
-import operate from '../logic/calculator.js';
 class Calculator extends Component {
   constructor(props){
     super();
@@ -9,7 +8,9 @@ class Calculator extends Component {
       total: null,
       next: null,
       operation: null,
-    }}
+    },
+    result:'',
+    }
 
     this.handleClick = this.handleClick.bind(this);
   }
@@ -18,11 +19,12 @@ class Calculator extends Component {
     const result = calculate(this.state.obj, e.target.name);
     this.setState({obj: {...this.state.obj, ...result}});
   }
+
   render() {
     const buttonsArray=['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
     return (
       <div className='Calculator'>
-        <p className = 'result'>{this.state.obj.total}</p>
+        <p className = 'result'>{(this.state.obj.total || '') + (this.state.obj.operation || '') + (this.state.obj.next || '')}</p>
         <div className='buttons'>
           {buttonsArray.map((btn) =><button key={btn} onClick={(e)=>this.handleClick(e)} name={btn}>{btn}</button>)}
         </div>
