4621003b054f45b7aaacc7c7bdd20f1ce4ccc368
Tolibjon <tolibjohn@gmail.com>
2021-08-17 10:56:47 +0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
-const App = () => <Calculator />;
+const App = () => (
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/calculator">
+        <Calculator />
+      </Route>
+      <Route path="/quote">
+        <Quote />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
